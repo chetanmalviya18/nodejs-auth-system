6503eee56c41eee8d5e7c8b5170855a76953d8c3
chetanmalviya18 <chetanmalviya.182@gmail.com>
2026-05-15 22:36:58 +0530

    Implement Access Token , Refresh Token, & Token Rotation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -29,15 +29,26 @@ export async function register(req: Request, res: Response) {
     password: hasedPassword,
   });
 
-  const token = jwt.sign({ id: newUser._id }, config.JWT_SECRET, {
+  const accessToken = jwt.sign({ id: newUser._id }, config.JWT_SECRET, {
     expiresIn: "1d",
+  });
+
+  const refreshToken = jwt.sign({ id: newUser._id }, config.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.status(201).json({
     success: true,
     message: "User registered successfully",
     user: { username: newUser.username, email: newUser.email },
-    token,
+    accessToken,
   });
 }
 
@@ -53,7 +64,7 @@ export async function getMe(req: Request, res: Response) {
 
   const decode = jwt.verify(token, config.JWT_SECRET);
 
-  if (typeof decode === 'string' || !('id' in decode)) {
+  if (typeof decode === "string" || !("id" in decode)) {
     return res.status(401).json({
       success: false,
       message: "Invalid token",
@@ -73,5 +84,54 @@ export async function getMe(req: Request, res: Response) {
     success: true,
     message: "User found",
     user: { username: user.username, email: user.email },
+  });
+}
+
+export async function refreshToken(req: Request, res: Response) {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(401).json({
+      success: false,
+      message: "No refresh token provided",
+    });
+  }
+
+  const decoded = jwt.verify(refreshToken, config.JWT_SECRET);
+
+  if (typeof decoded === "string" || !("id" in decoded)) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid refresh token",
+    });
+  }
+
+  const user = await userModel.findById(decoded.id);
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  const newRefreshToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  const newAccessToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+
+  res.cookie("refreshToken", newRefreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Access token refreshed successfully",
+    accessToken: newAccessToken,
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import config from "./config";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db";
 import authRouter from "./routes/auth.routes";
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
